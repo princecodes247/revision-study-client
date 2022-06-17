c813ee6687311138e101303bc2f3551662901ce8
Prince Codes <princecodes247@gmail.com>
2022-06-17 05:48:54 -0400

    Update home page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Header from '~/components/Header'
-import Footer from '~/components/Footer'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -12,32 +12,36 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <main className="min-h-screen flex justify-center items-center">
-        <section className='flex'>
-          <div className="flex-1">
-          <h1 className="text-4xl font-bold">RevisionStudy</h1>
-          <p className="text-xl">
-            A simple app to help you revise your vocabulary.
-          </p>
+      <main className="min-h-screen">
+        <section className="flex items-center h-screen">
+          <div className=" flex flex-col items-center justify-center flex-1">
+            <h1 className="text-7xl font-bold">RevisionStudy</h1>
+            <p className="text-xl">
+              A simple app to help you revise your vocabulary.
+            </p>
+            <button className="my-4 text-white py-3 px-6 rounded bg-blue-600">
+              Get Started!
+            </button>
+            <button className="py-3 px-6 rounded border border-gray-200">
+              Get Started!
+            </button>
           </div>
-          <div className="hero_img flex-1">
-
-          </div>
+          <div className="hero_img flex-1">djsosdfo</div>
         </section>
-        <section className='flex'>
-          <div className="">Features</div>
-          <div className="">Features</div>
-          <div className="">Features</div>
-          <div className="">Features</div>
-          <div className="">Features</div>
+        <section className="flex gap-4 w-full p-4">
+          {[1, 2, 3, 4, 5].map((i) => (
+            <div key={i} className="p-12 flex-1 text-center bg-gray-100">
+              Features
+            </div>
+          ))}
         </section>
-        <div className="banner_sect">
+        <div className="flex p-8 items-center justify-center">
           We are 1% for something
         </div>
       </main>
-      <Footer />  
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
